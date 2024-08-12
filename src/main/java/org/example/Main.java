fc727570ae229e0ab10220a292cd7b6d4757547c
Foxfnaf <foxyfnaf.93yt@gmail.com>
2024-08-12 19:51:00 +0200

    commit

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,8 +2,11 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-        FileMenager fileMenager = new FileMenager();
 
-        fileMenager.
+        try{
+            FileManager.inputFile();
+        }catch (NullPointerException exception){
+            FileManager.inputFile();
+        }
     }
 }
