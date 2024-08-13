0791c86049c649077b05f060523da5db0faf0af3
Foxfnaf <foxyfnaf.93yt@gmail.com>
2024-08-13 20:10:09 +0200

    commit

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,12 +1,49 @@
 package org.example;
 
-public class Main {
-    public static void main(String[] args) {
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Scanner;
 
-        try{
-            FileManager.inputFile();
-        }catch (NullPointerException exception){
-            FileManager.inputFile();
-        }
+public class Main {
+    public static final InputStream INPUT_STREAM = System.in;
+    public static final Scanner INPUT_USER_READER = new Scanner(INPUT_STREAM);
+
+    public static void main(String[] args) throws  IOException {
+        String filePath = "";
+
+        filePath = askUserForFilePath(filePath, INPUT_USER_READER);
+
+        checkIfFileExist(filePath);
+        printFileContent(INPUT_USER_READER, filePath);
     }
+
+    public static String askUserForFilePath(String filePath, Scanner fileScanner) {
+
+        System.out.println("Podaj scieżkę pliku");
+        filePath = fileScanner.nextLine();
+        return filePath;
+    }
+
+    public static boolean checkIfFileExist(String filePath) {
+        File inputFile = new File(filePath);
+        boolean result = inputFile.exists();
+        return result;
+    }
+
+    public static boolean printFileContent(Scanner fileScanner, String filePath) {
+        try {
+            fileScanner = new Scanner(filePath);
+        } catch (FileNotFoundException e) {
+            System.out.println("Podany plik nie istnieje nie istnieje");
+            return false;
+        }
+        while (fileScanner.hasNext()) {
+            System.out.println(fileScanner.nextLine());
+        }
+
+        return true;
+    }
+
 }
