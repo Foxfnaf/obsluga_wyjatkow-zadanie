527eca32be81c1786b9da8d7c631edd593e7d213
Foxfnaf <foxyfnaf.93yt@gmail.com>
2024-08-13 21:10:06 +0200

    commit

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,47 +1,76 @@
 package org.example;
 
 import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.IOException;
-import java.io.InputStream;
+
+import java.io.*;
 import java.util.Scanner;
 
 public class Main {
+
     public static final InputStream INPUT_STREAM = System.in;
-    public static final Scanner INPUT_USER_READER = new Scanner(INPUT_STREAM);
 
-    public static void main(String[] args) throws  IOException {
-        String filePath;
+    private static final Scanner INPUT_SCANNER = new Scanner(INPUT_STREAM);
 
-        filePath = askUserForFilePath(INPUT_USER_READER);
+    public static void main(final String[] args) throws IOException {
+        String inputFileName;
+        do {
+            inputFileName = askUserForFileName(INPUT_SCANNER);
+        } while (!checkIsFileExist(inputFileName));
 
-        checkIfFileExist(filePath);
-        printFileContent(INPUT_USER_READER, filePath);
+        printFileContent(inputFileName);
+
+        String additionalText = askUserForAdditionalText();
+
+        String outputFileName = inputFileName;
+        FileWriter fileWriter = null;
+
+        try {
+            fileWriter = new FileWriter(outputFileName, true);
+            fileWriter.write(additionalText);
+        }finally {
+            if(fileWriter != null) {
+                fileWriter.close();
+            }
+        }
+        printFileContent(outputFileName);
     }
-
-    public static String askUserForFilePath(Scanner fileScanner) {
-        System.out.println("Podaj scieżkę pliku");
-        return fileScanner.nextLine();
-    }
-
-    public static boolean checkIfFileExist(String filePath) {
-        File inputFile = new File(filePath);
+    private static boolean checkIsFileExist(final String fileName){
+        File inputFile = new File(fileName);
         boolean result = inputFile.exists();
+        if(!result) {
+            System.out.println("Podany plik ["+ inputFile +"] nie istnieje");
+        }
         return result;
     }
 
-    public static boolean printFileContent(Scanner fileScanner, String filePath) {
+    private static String askUserFor(final String question){
+        System.out.println(question);
+
+        return INPUT_SCANNER.nextLine();
+    }
+
+    private static String askUserForAdditionalText(){
+        return askUserFor("Podaj tekst który chcesz wpisać(usunie to całą zawartość pliku)");
+    }
+
+    private static String askUserForFileName(final Scanner scanner){
+        return askUserFor("Podaj ścieżkę pliku");
+    }
+
+    private static Boolean printFileContent(final String inputFileName){
+        File inputFile = new File(inputFileName);
+        Scanner fileScanner = null;
+
         try {
-            fileScanner = new Scanner(filePath);
-        } catch (FileNotFoundException e) {
-            System.out.println("Podany plik nie istnieje nie istnieje");
+            fileScanner = new Scanner(inputFile);
+        }catch (FileNotFoundException e){
+            System.out.println("Podany plik ["+ inputFile +"] nie istnieje");
             return false;
         }
-        while (fileScanner.hasNext()) {
+        while (fileScanner.hasNext()){
             System.out.println(fileScanner.nextLine());
         }
 
         return true;
     }
-
 }
